f818cc41de31a833c7ed1acdd4e591a6f2532b7c
fish <fish@yuyuandeMacBook-Pro.local>
2019-03-25 17:03:53 +0800

    1.本地第一次提交,添加工程文件;

---
+++ DetailInfoPodModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DetailInfoPodModule
+//
+//  Created by fish on 2019/3/25.
+//  Copyright © 2019 fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DetailInfoPodModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DetailInfoPodModule
+//
+//  Created by fish on 2019/3/25.
+//  Copyright © 2019 fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
