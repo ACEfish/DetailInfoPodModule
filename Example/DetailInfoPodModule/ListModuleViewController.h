41e078dbff7f3f236744c880eea08ebf3a26c2f5
fish <fish@yuyuandeMacBook-Pro.local>
2019-03-26 20:28:08 +0800

    Release 0.0.2

---
+++ Example/DetailInfoPodModule/ListModuleViewController.h
@@ -0,0 +1,17 @@
+//
+//  ListModuleViewController.h
+//  DetailInfoPodModule
+//
+//  Created by fish on 2019/3/26.
+//  Copyright © 2019 fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ListModuleViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
